e10ea861de853b6d2d900308aed5084127fe2602
lxszship <lxszship135@126.com>
2024-11-27 23:12:29 +0800

    init

--- data-center/src/main/java/ppl/server/iam/data/controller/authn/UserController.java
+++ data-center/src/main/java/ppl/server/iam/data/controller/authn/UserController.java
@@ -1,13 +1,13 @@
 package ppl.server.iam.data.controller.authn;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ppl.server.base.webmvc.response.jackson.JController;
 import ppl.server.iam.authn.UserService;
 import ppl.server.iam.authn.bo.EnhancedUser;
 
-@Controller
+@JController
 public class UserController {
     @Autowired
     private UserService userService;
